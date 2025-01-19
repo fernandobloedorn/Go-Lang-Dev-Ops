390b83c4ab15dcb4f71af2ea4a491e6ea26f398f
fernandobloedorn <fernandobloedorn@gmail.com>
2025-01-19 06:53:30 -0300

    feat: add routes

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/fernandobloedorn/Go-Lang-Dev-Ops/router"
+
+func main() {
+	router.Initialize()
+}
---
+++ main_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func test() {
+	fmt.Print("Teste para fazer no futuro")
+}
