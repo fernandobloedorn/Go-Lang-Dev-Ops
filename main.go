a6a4d103c1dfc5313762769d34bd8abd6f9a4977
fernandobloedorn <fernandobloedorn@gmail.com>
2025-01-20 05:44:04 -0300

    feat: add opening schema

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/fernandobloedorn/Go-Lang-Dev-Ops/router"
+import (
+	"github.com/fernandobloedorn/Go-Lang-Dev-Ops/config"
+	"github.com/fernandobloedorn/Go-Lang-Dev-Ops/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+
+	logger := config.GetLogger("main")
+
+	err := config.Init()
+	if err == nil {
+		logger.Errorf("Config initialization error: %v", err)
+		// panic(err)
+		return
+	}
+
 	router.Initialize()
 }
